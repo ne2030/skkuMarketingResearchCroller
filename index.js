615d385f9c36e12f741921331b3072d490c7b6b4
Ryan <erguono@naver.com>
2018-04-05 12:47:13 +0900

    index 주석처리 - lamda 업로드 잠정 중단

--- index.js
+++ index.js
@@ -1,102 +1,64 @@
-const path = require('path');
-const childProcess = require('child_process');
-const Slack = require('slack-node');
-const fs = require('fs');
-const _ = require('partial-js');
+// aws lamda 업로드 준비 중 - phantom js 를 lamda 에서 돌리는 법을 찾고 있습니다 ...
 
-const { difference, uniqObjectArray, stopIf, strJoin } = require('./fpUtils');
+// const path = require('path');
+// const childProcess = require('child_process');
+// const Slack = require('slack-node');
+// const fs = require('fs');
+// const _ = require('partial-js');
 
-const slackUrl = 'https://hooks.slack.com/services/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';
-const slack = new Slack();
-slack.setWebhook(slackUrl);
+// const { difference, uniqObjectArray, stopIf, strJoin } = require('./fpUtils');
 
-const data = require('./data')
+// const slack = new Slack();
+// slack.setWebhook(process.env.SLACK_URL);
 
-exports.handler = function(event, context) {
+// const data = require('./data')
 
-    // Set the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
-    process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+// exports.handler = function(event, context) {
 
-    // Set the path to the phantomjs binary
-    var phantomPath = path.join(__dirname, 'phantomjs_linux-x86_64');
+//     // Set the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
+//     process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-    // Arguments for the phantom script
-    var processArgs = [
-        path.join(__dirname, 'phantom-script.js'),
-       'my arg'
-    ];
+//     // Set the path to the phantomjs binary
+//     var phantomPath = path.join(__dirname, 'phantomjs_linux-x86_64');
 
-    const childArgs = [ path.join(__dirname, 'phantomjs-script.js')];
+//     // Arguments for the phantom script
+//     var processArgs = [
+//         path.join(__dirname, 'phantom-script.js'),
+//        'my arg'
+//     ];
 
-    childProcess.execFile(phantomPath, processArgs, function(err, stdout, stderr) {
-        if (err) {
-            console.log('error!!', err);
-        }
+//     const childArgs = [ path.join(__dirname, 'phantomjs-script.js')];
 
-        const list = JSON.parse(stdout);
+//     childProcess.execFile(phantomPath, processArgs, function(err, stdout, stderr) {
+//         if (err) {
+//             console.log('error!!', err);
+//         }
 
-        const isSameNotice = (prev, cur) => prev.title === cur.title && prev.date === cur.date;
+//         const list = JSON.parse(stdout);
 
-        const storeAllNotices = _.pipe(
-            newlist => _.union(list, data),
-            uniqObjectArray(isSameNotice),
-            JSON.stringify,
-            notices => fs.writeFile('data.json', notices, (err) => {
-                if (err) console.log(err);
-            })
-        );
+//         const isSameNotice = (prev, cur) => prev.title === cur.title && prev.date === cur.date;
 
-        const slackNewNotices = (list) => {
-            console.log(list);
-            slack.webhook({
-                text: strJoin('\n')(_.map(list, notice => `제목: ${notice.title}, 일자: ${notice.date}`))
-            }, function(err, response) {});
-        }
+//         const storeAllNotices = _.pipe(
+//             newlist => _.union(list, data),
+//             uniqObjectArray(isSameNotice),
+//             JSON.stringify,
+//             notices => fs.writeFile('data.json', notices, (err) => {
+//                 if (err) console.log(err);
+//             })
+//         );
 
-        _.go(list,
-            _.partial(difference, _, data, isSameNotice),
-            stopIf(_.isEmpty),
-            slackNewNotices,
-            storeAllNotices
-        );
-    });
-}
+//         const slackNewNotices = (list) => {
+//             console.log(list);
+//             slack.webhook({
+//                 text: strJoin('\n')(_.map(list, notice => `제목: ${notice.title}, 일자: ${notice.date}`))
+//             }, function(err, response) {});
+//         }
 
-function test() {
-    // unique object array - function test
-
-    {
-        const uniqArray = uniqObjectArray(
-            [{ a: 10, b: 20}, { a: 10, b: 30 }, { a: 20, b: 40 }, { a: 50, b: 60 }, { a: 30, b: 100}, { a: 50, b: 100}],
-            (prev, cur) => prev.a === cur.a
-        );
-        
-        const expectedResult = [ { a: 10, b: 20 },
-            { a: 20, b: 40 },
-            { a: 50, b: 60 },
-            { a: 30, b: 100 }];
-        
-        console.log('\n\n unique test results :: ', JSON.stringify(uniqArray) === JSON.stringify(expectedResult));
-    }
-
-    // difference object array - function test
-
-    {
-        const objArray = [{ "title": "마케팅 소비자 조사", "date" : "2018-03-31"}, 
-            {"title":"마케팅 실험2","date":"2018-03-23"},
-            {"title":"마케팅실험","date":"2018-03-16"},
-            {"title":"마케팅 실험1","date":"2018-03-12"}
-        ];
-
-        const removalArray = [{ "title": "마케팅 소비자 조사", "date" : "2018-03-31"}, 
-            {"title":"마케팅실험","date":"2018-03-16"},
-        ];
-
-        const removalResult = difference(objArray, removalArray, (prev, cur) => prev.title === cur.title && prev.date === cur.date);
-        
-        const expectedResult = [{"title":"마케팅 실험2","date":"2018-03-23"}, {"title":"마케팅 실험1","date":"2018-03-12"}];
-
-        console.log('\n\n difference test results :: ', JSON.stringify(removalResult) === JSON.stringify(expectedResult));
-    }
-}
-
+//         _.go(list,
+//             _.partial(difference, _, data, isSameNotice),
+//             stopIf(_.isEmpty),
+//             slackNewNotices,
+//             storeAllNotices
+//         );
+//     });
+// }
